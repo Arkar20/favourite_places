f5025dfadb6c00aa4562aa5cf088cb5db408f085
Arkar <69750819+Arkar20@users.noreply.github.com>
2023-07-05 00:29:00 +0630

    adjust text color in textfiled

--- lib/screens/CreatePlace.dart
+++ lib/screens/CreatePlace.dart
@@ -1,13 +1,23 @@
+import 'package:favourite_places/store/PlaceProvider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CreatePlace extends StatefulWidget {
+class CreatePlace extends ConsumerStatefulWidget {
   const CreatePlace({super.key});
 
   @override
-  State<CreatePlace> createState() => _CreatePlaceState();
+  ConsumerState<CreatePlace> createState() => _CreatePlaceState();
 }
 
-class _CreatePlaceState extends State<CreatePlace> {
+class _CreatePlaceState extends ConsumerState<CreatePlace> {
+
+  final _textController=TextEditingController();
+
+  @override
+  void dispose(){
+     _textController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +30,11 @@ class _CreatePlaceState extends State<CreatePlace> {
             child: Column(
           children: [
             TextFormField(
+              controller: _textController,
               decoration: const InputDecoration(label: Text("Name a place")),
+               style: TextStyle(
+                color:Theme.of(context).colorScheme.onBackground
+               )
             ),
             const SizedBox(height: 24,),
             Row(
@@ -28,7 +42,9 @@ class _CreatePlaceState extends State<CreatePlace> {
               children: [
                 ElevatedButton.icon(
                   onPressed: () {
-                    print('print');
+                    final text=_textController.text;
+                    ref.read(placeProvider.notifier).addPlace(text);
+                    Navigator.of(context).pop();
                   },
                   icon: const Icon(Icons.add_a_photo_outlined),
                   label: const Text(
