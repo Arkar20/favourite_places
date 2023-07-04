776fb6279817a1884b60427cbae489fc446dd62a
Arkar <69750819+Arkar20@users.noreply.github.com>
2023-07-04 23:53:58 +0630

    list places

--- lib/screens/PlaceList.dart
+++ lib/screens/PlaceList.dart
@@ -1,3 +1,4 @@
+import 'package:favourite_places/models/Place.dart';
 import 'package:favourite_places/screens/CreatePlace.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +10,11 @@ class PlaceList extends StatefulWidget {
 }
 
 class _PlaceListState extends State<PlaceList> {
+
+  List<Place> _places=[
+    Place(id: '1', title: "hello world"),
+    Place(id: '2', title: "hello world")
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +25,16 @@ class _PlaceListState extends State<PlaceList> {
             Navigator.of(context).push(MaterialPageRoute(builder: (context)=> CreatePlace() ));
           }, icon: const Icon(Icons.add))
         ],
+      ),
+      body: ListView.builder(
+        itemCount: _places.length,
+        itemBuilder:  (context, index) => ListTile(
+          leading:Text(_places[index].title,style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+            color: Theme.of(context).colorScheme.onBackground,
+            fontSize: 20
+          ),),
+        ),
+
       ),
     );
   }
