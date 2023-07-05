6bfd8d3c019ec80b92175bc0ef12ddae4ee52495
Arkar <69750819+Arkar20@users.noreply.github.com>
2023-07-05 14:15:49 +0630

    image picker fun

--- lib/widgets/ImageInput.dart
+++ lib/widgets/ImageInput.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   const ImageInput({super.key});
@@ -8,22 +11,47 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
+  File? _image;
+
+  void takeImage() async {
+// ImagePicker.pickImage();
+    final picker = ImagePicker();
+    final image = await picker.pickImage(
+        source: ImageSource.camera, maxWidth: double.infinity);
+
+    if (image == null) {
+      return;
+    }
+
+    setState(() {
+      _image = File(image.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    Widget content= TextButton.icon(
+        onPressed: () {
+          takeImage();
+        },
+        icon: const Icon(Icons.camera),
+        label: const Text("Take Camera"));
+
+        if(_image!=null){
+          content= Image.file(_image!,fit:BoxFit.cover);
+        }
     return Container(
-      padding: const  EdgeInsets.all(16),
+      padding: const EdgeInsets.all(16),
       decoration: BoxDecoration(
-        border: Border.all(width: 1,color: Theme.of(context).colorScheme.primary.withOpacity(0.2))
-      ),
+          border: Border.all(
+              width: 1,
+              color: Theme.of(context).colorScheme.primary.withOpacity(0.2))),
       height: 200,
       width: double.infinity,
-      child: TextButton.icon(
-        onPressed: (){
-
-        },
-        icon: Icon(Icons.camera),
-        label: Text("Take Camera")
-      ),
+      child: content,
     );
   }
+
+ 
 }
